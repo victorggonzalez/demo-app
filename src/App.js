6e773e23d655d89442ad1dea3fa0c7a3fd241b58
Víctor González <36533965+victorggonzalez@users.noreply.github.com>
2022-07-06 17:18:41 +0200

    fix: test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload changes in this test app testing invalidations. 
+          Edit <code>src/App.js</code> and save to changes in this test app testing invalidations. 
         </p>
         <a
           className="App-link"
